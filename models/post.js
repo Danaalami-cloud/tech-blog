28ef55c9ddae2000a843c3e27db98adba6f7126d
Dana Alami <dana.alami18@gmail.com>
2021-06-02 19:20:09 +0300

    sequelized contents

---
+++ models/post.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes} = require("sequilize");
+const sequilize = require("../config/connections");
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            refrences: {
+                model: 'user',
+                key: 'id'
+            }
+
+        }
+
+    },
+),
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'project',
+  }
+
+
+module.exports = Post;
