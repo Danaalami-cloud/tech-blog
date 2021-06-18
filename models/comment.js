e0f560a84f73968aa270fb83a7cb323d3e4c474f
Dana Alami <dana.alami18@gmail.com>
2021-06-18 13:03:49 +0300

    contents added to index,post and user files

---
+++ models/comment.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes} = require("sequilize");
+const sequilize = require("../config/connections");
+
+class Comment extends Model {}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            refrences: {
+                model: 'user',
+                key: 'id'
+            }
+
+        }
+
+    },
+),
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'post',
+  }
+
+
+module.exports = Comment;
